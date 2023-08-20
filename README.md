80ea58f6c2ec6c8f6ddad1624ebdb74481aab296
Thomas Schaller <me@torkleyy.com>
2023-08-20 17:20:09 +0200

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# xsd-generator
+
+Generate Rust code from XML Schema.
+
+* [x] Root Types
+* [x] Root Elements
+* [x] `element`
+    * [x] nested `complexType`
+    * [x] nested `simpleType`
+    * [x] referenced `@type`
+* [x] `complexType`
+    * [x] nested `all`
+    * [x] nested `seq`
+    * [ ] nested `choice`
+* [x] `simpleType`
+    * [x] nested `restriction`
+        * [x] nested `enmeration`
+    * [x] referenced `@base`
+
+# License
+
+This project is licensed under Apache-2.0.
---
+++ src/main.rs
@@ -0,0 +1,259 @@
+use serde::Deserialize;
+use std::{fs, fmt::Write};
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct XsdSchema {
+    #[serde(rename = "element")]
+    #[serde(default)]
+    elements: Vec<XsdElement>,
+    #[serde(rename = "complexType")]
+    #[serde(default)]
+    complex_types: Vec<XsdComplexType>,
+    #[serde(rename = "simpleType")]
+    #[serde(default)]
+    simple_types: Vec<XsdSimpleType>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct XsdElement {
+    #[serde(rename = "@name")]
+    name: String,
+    #[serde(rename = "@type")]
+    data_type: Option<String>,
+    #[serde(rename = "complexType")]
+    complex_type: Option<XsdComplexType>,
+    #[serde(rename = "simpleType")]
+    simple_type: Option<XsdSimpleType>,
+}
+
+impl XsdElement {
+    pub fn rs_type_name(&self) -> String {
+        if self.complex_type.is_some() || self.simple_type.is_some() {
+            to_pascal_case(&self.name)
+        } else {
+            map_to_rust_type(self.data_type.as_ref().unwrap())
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct XsdComplexType {
+    #[serde(rename = "@name")]
+    name: Option<String>,
+    #[serde(rename = "sequence")]
+    sequence: Option<Sequence>,
+    #[serde(rename = "choice")]
+    choice: Option<Choice>,
+    #[serde(rename = "all")]
+    all: Option<All>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Sequence {
+    #[serde(rename = "element")]
+    elements: Vec<XsdElement>,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct Choice {
+    #[serde(rename = "element")]
+    elements: Vec<XsdElement>,
+}
+
+#[derive(Debug, Deserialize)]
+struct All {
+    #[serde(rename = "element")]
+    elements: Vec<XsdElement>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct XsdSimpleType {
+    #[serde(rename = "@name")]
+    name: Option<String>,
+    restriction: Option<Restriction>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Restriction {
+    #[serde(rename = "@base")]
+    base: String,
+    enumeration: Option<Vec<Enumeration>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Enumeration {
+    #[serde(rename = "@value")]
+    value: String,
+}
+
+fn generate_element(elem: XsdElement, f: &mut String) {
+    if let Some(mut ty) = elem.complex_type {
+        ty.name = Some(elem.name.clone());
+
+        generate_complex(ty, f);
+    } else if let Some(mut ty) = elem.simple_type {
+        ty.name = Some(elem.name.clone());
+
+        generate_simple(ty, f);
+    }
+}
+
+fn generate_complex(ty: XsdComplexType, f: &mut String) {
+    let name = to_pascal_case(&ty.name.unwrap());
+
+    if let Some(all) = ty.all {
+        generate_struct(&name, all.elements, f);
+    } else if let Some(_choice) = ty.choice {
+        unimplemented!()
+    } else if let Some(seq) = ty.sequence {
+        generate_struct(&name, seq.elements, f);
+    }
+}
+
+fn generate_simple(ty: XsdSimpleType, f: &mut String) {
+    let name = to_pascal_case(&ty.name.unwrap());
+
+    if let Some(restr) = ty.restriction {
+        if let Some(en) = restr.enumeration.filter(|en| en.iter().all(|v| v.value.chars().next().map(|c| c.is_ascii_alphabetic()).unwrap_or(false))) {
+            let rs_type = map_to_rust_type(&restr.base);
+            let _ = writeln!(f, "#[derive(Clone, Debug, Deserialize, Serialize)]");
+            let _ = writeln!(f, "pub enum {rs_type} {{");
+            for v in en {
+                let xml_name = &v.value;
+                let v_ident = to_pascal_case(xml_name);
+                let _ = writeln!(f, "#[serde(rename = \"{xml_name}\")]");
+                let _ = writeln!(f, "{v_ident}");
+            }
+            let _ = writeln!(f, "}}");
+        } else {
+            let rs_type = map_to_rust_type(&restr.base);
+            let _ = writeln!(f, "pub type {name} = {rs_type};");
+        }
+    } else {
+        let _ = writeln!(f, "pub type {name} = String;");
+    }
+}
+
+fn generate_struct(name: &str, elements: Vec<XsdElement>, f: &mut String) {
+    let _ = writeln!(f, "#[derive(Clone, Debug, Deserialize, Serialize)]");
+    let _ = writeln!(f, "pub struct {name} {{");
+
+    for elem in &elements {
+        let xml_name = &elem.name;
+        let rs_name = to_snake_case(&elem.name);
+        let ty_name = elem.rs_type_name();
+        let _ = writeln!(f, "#[serde(rename = \"{xml_name}\")]");
+        let _ = writeln!(f, "pub {rs_name}: {ty_name},");
+    }
+
+    let _ = writeln!(f, "}}");
+
+    for elem in elements {
+        generate_element(elem, f);
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let xml_content = fs::read_to_string("schema.xsd")?;
+    let xsd_schema: XsdSchema = quick_xml::de::from_str(&xml_content)?;
+
+    let mut buf = String::new();
+
+    for ty in xsd_schema.complex_types {
+        generate_complex(ty, &mut buf);
+    }
+
+    for ty in xsd_schema.simple_types {
+        generate_simple(ty, &mut buf);
+    }
+    
+    for elem in xsd_schema.elements {
+        generate_element(elem, &mut buf);
+    }
+
+    println!("{buf}");
+    
+    Ok(())
+}
+
+fn map_to_rust_type(xsd_type: &str) -> String {
+    if let Some(ty) = xsd_type.strip_prefix("xs:").or_else(|| xsd_type.strip_prefix("xsd:")) {
+        map_primitive_to_rust_type(ty).to_owned()
+    } else if !xsd_type.is_empty() {
+        to_pascal_case(xsd_type)
+    } else {
+        "String".to_owned()
+    }
+}
+
+/// assumes no prefix
+fn map_primitive_to_rust_type(prim_type: &str) -> &'static str {
+    match prim_type {
+        "string" | "normalizedString" | "token" | "language" | "Name" | "NMTOKEN" => "String",
+        "boolean" => "bool",
+        "decimal" => "f64",
+        "float" => "f32",
+        "double" => "f64",
+        "integer" => "i64",
+        "nonPositiveInteger" => "i64",
+        "negativeInteger" => "i64",
+        "long" => "i64",
+        "int" => "i32",
+        "short" => "i16",
+        "byte" => "i8",
+        "nonNegativeInteger" => "u64",
+        "unsignedLong" => "u64",
+        "unsignedInt" => "u32",
+        "unsignedShort" => "u16",
+        "unsignedByte" => "u8",
+        "positiveInteger" => "u64",
+        "dateTime" | "time" | "date" | "gYearMonth" | "gYear" | "gMonthDay" | "gDay" | "gMonth" | "duration" => "String", // might change to chrono type
+        _ => "String", // Default to String for unknown types
+    }
+}
+
+fn to_pascal_case(input: &str) -> String {
+    let mut result = String::new();
+    let mut capitalize_next = true;
+
+    for c in input.chars() {
+        if c == '_' || c == '-' {
+            capitalize_next = true;
+        } else if c.is_ascii_alphanumeric() {
+            if capitalize_next {
+                result.push(c.to_ascii_uppercase());
+                capitalize_next = false;
+            } else {
+                result.push(c);
+            }
+        }
+    }
+
+    result
+}
+
+fn to_snake_case(input: &str) -> String {
+    let mut result = String::new();
+    let mut prev_char = '\0';
+
+    for c in input.chars() {
+        if c.is_ascii_uppercase() {
+            if !prev_char.is_ascii_uppercase() && prev_char != '\0' {
+                result.push('_');
+            }
+            result.push(c.to_ascii_lowercase());
+        } else if c == '-' {
+            result.push('_');
+        } else {
+            result.push(c);
+        }
+        prev_char = c;
+    }
+
+    result
+}
